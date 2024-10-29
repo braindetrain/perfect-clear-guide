697114ff5c8019799b697e4a2cd480d41d024d70
Brian Tran <135712063+braindetrain@users.noreply.github.com>
2024-10-29 08:44:52 -0400

    Create ilsz.h

---
+++ ilsz.h
@@ -0,0 +1,29 @@
+// ilsz.h
+#ifndef ILSZ_H
+#define ILSZ_H
+
+int ILSZ(char queue[]);
+int ILSZSolution1(char heldPiece, char queue[], int queueSize);
+int ILSZSolution2(char heldPiece, char queue[], int queueSize);
+int ILSZSolution3(char heldPiece, char queue[], int queueSize);
+int ILSZSolution4(char heldPiece, char queue[], int queueSize);
+int ILSZSolution5(char heldPiece, char queue[], int queueSize);
+int ILSZSolution6(char heldPiece, char queue[], int queueSize);
+int ILSZSolution7(char heldPiece, char queue[], int queueSize);
+int ILSZSolution8(char heldPiece, char queue[], int queueSize);
+int ILSZSolution9(char heldPiece, char queue[], int queueSize);
+int ILSZSolution10(char heldPiece, char queue[], int queueSize);
+int ILSZSolution11(char heldPiece, char queue[], int queueSize);
+int ILSZSolution12(char heldPiece, char queue[], int queueSize);
+int ILSZSolution13(char heldPiece, char queue[], int queueSize);
+int ILSZSolution14(char heldPiece, char queue[], int queueSize);
+int ILSZSolution15(char heldPiece, char queue[], int queueSize);
+int ILSZSolution16(char heldPiece, char queue[], int queueSize);
+int ILSZSolution17(char heldPiece, char queue[], int queueSize);
+int ILSZSolution18(char heldPiece, char queue[], int queueSize);
+int ILSZSolution19(char heldPiece, char queue[], int queueSize);
+int ILSZSolution20(char heldPiece, char queue[], int queueSize);
+int ILSZSolution21(char heldPiece, char queue[], int queueSize);
+int ILSZSolution22(char heldPiece, char queue[], int queueSize);
+
+#endif // ILSZ_H
